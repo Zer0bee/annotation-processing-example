e2dc030993e6512407012da8200136024f2c090d
Manish Sharma <manishshrm330@gmail.com>
2019-06-28 12:25:40 +0530

    init commit

---
+++ app/src/main/java/com/zer0bee/annotationprocessingexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zer0bee.annotationprocessingexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
