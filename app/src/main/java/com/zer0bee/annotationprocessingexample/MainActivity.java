378a31302ade5cfc2e3a0fa45c01f4fbcbda2db8
Manish Sharma <manishshrm330@gmail.com>
2019-07-10 16:26:40 +0530

    plug binding class generated through processor to activity using another Binding class

--- app/src/main/java/com/zer0bee/annotationprocessingexample/MainActivity.java
+++ app/src/main/java/com/zer0bee/annotationprocessingexample/MainActivity.java
@@ -2,8 +2,11 @@ package com.zer0bee.annotationprocessingexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import com.zer0bee.annotations.BindView;
+import com.zer0bee.binder.Binding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +17,11 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    //button.setOnClickListener(new View.OnClickListener() {
-    //  @Override public void onClick(View v) {
-    //    Toast.makeText(ReflectionExampleActivity.this, "Hello World!!", Toast.LENGTH_SHORT).show();
-    //  }
-    //});
+    Binding.bind(this);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Toast.makeText(MainActivity.this, "Hello World!!", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 }
